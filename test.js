3223a838e0b027cbc247bfdb5109499b68a82066
Dimitri Pirogov <dimpirogovp@hotmail.com>
2020-12-16 14:38:25 +0100

    3 files, 1 empty 201216_1438

--- test.js
+++ test.js
@@ -1,1 +1,14 @@
-//test new file
+//------------------
+//login page JS-----
+//------------------
+function check(form){
+    if(form.userNameInput.value == "test" && form.passwordInput.value == "cirkus"){
+        window.open('about.html');
+    }
+    else{
+        alert("Error Password or User name");
+    }
+}
+//--------------------
+//login page JS ends--
+//--------------------
