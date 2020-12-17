26a36535adfc2f16b59968fd7011fe3b590117b9
Dimitri Pirogov <dimpirogovp@hotmail.com>
2020-12-17 15:43:15 +0100

    201217 15:43

--- login.js
+++ login.js
@@ -3,12 +3,24 @@
 //------------------
 function check(form){
     if(form.userNameInput.value == "admin" && form.passwordInput.value == "cirkus"){
-        window.open('about.html');
+        window.open('admin.html');
     }
     else{
         alert("Error Password or User name");
     }
 }
+var checkboxesTable1 = document.querySelectorAll("input[type=checkbox][name=table1");
+let enabledSetting = [];
+
+checkboxesTable1.forEach(function(checkbox){
+    checkbox.addEventListener('change',function(){
+        enabledSetting = 
+            Array.from(checkboxesTable1)    //Convert checkboxes to an array to use filter and map
+                .filter
+
+    })
+})
+
 //--------------------
 //login page JS ends--
 //--------------------
